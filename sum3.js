88e96eb639d29a765c31a7e640c832b505162857
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-07 22:59:38 +0100

    solved

--- sum3.js
+++ sum3.js
@@ -6,3 +6,10 @@ Examples
 sum3([1, 2, 3]) → 6
 sum3([5, 11, 2]) → 18
 sum3([7, 0, 0]) → 7
+
+function sum3(arr){
+    let sum = 0
+  for(let i =0; i< arr.length; i++){
+  sum+= arr[i]
+  }return sum
+  }
