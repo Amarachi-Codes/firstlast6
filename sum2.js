31b6dfbbe5f0ce698f6ec27cd21460f72d6fac83
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-07 23:07:43 +0100

    solved

--- sum2.js
+++ sum2.js
@@ -1,9 +1,21 @@
-Array-1 -- sum2
-Given an array of ints, return the sum of the first 2 elements in the array. If the array length 
-is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+// Array-1 -- sum2
+// Given an array of ints, return the sum of the first 2 elements in the array. If the array length 
+// is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
 
-Examples
+// Examples
 
-sum2([1, 2, 3]) → 3
-sum2([1, 1]) → 2
-sum2([1, 1, 1, 1]) → 2
+// sum2([1, 2, 3]) → 3
+// sum2([1, 1]) → 2
+// sum2([1, 1, 1, 1]) → 2
+
+function sum2(nums){
+    if(nums.length === 0){
+    
+    return 0
+    }if(nums.length === 1){
+    return nums[0]
+    }
+    let sum =0
+    sum=nums[0]+nums[0+1]
+    return sum
+  }
