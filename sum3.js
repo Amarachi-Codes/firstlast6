b0a8c2f7a03beb40e6d26644b581f018755ddf28
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-08 22:17:25 +0100

    updated question

--- sum3.js
+++ sum3.js
@@ -1,11 +1,11 @@
-Array-1 -- sum3
-Given an array of ints length 3, return the sum of all the elements.
+// Array-1 -- sum3
+// Given an array of ints length 3, return the sum of all the elements.
 
-Examples
+// Examples
 
-sum3([1, 2, 3]) → 6
-sum3([5, 11, 2]) → 18
-sum3([7, 0, 0]) → 7
+// sum3([1, 2, 3]) → 6
+// sum3([5, 11, 2]) → 18
+// sum3([7, 0, 0]) → 7
 
 function sum3(arr){
     let sum = 0
