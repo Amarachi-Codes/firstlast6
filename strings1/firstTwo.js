c2b333035d57ae8f0f09f28986b94adcc2ad9d65
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-08 22:43:04 +0100

    updated

--- strings1/firstTwo.js
+++ strings1/firstTwo.js
@@ -1,10 +1,18 @@
-String-1 -- firstTwo
-Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
-If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty 
-string "" yields the empty string "". Note that str.length() returns the length of a string.
+// String-1 -- firstTwo
+// Given a string, return the string made of its first two chars, so the String "Hello" yields "He". 
+// If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty 
+// string "" yields the empty string "". Note that str.length() returns the length of a string.
 
-Examples
+// Examples
 
-firstTwo('Hello') → He
-firstTwo('abcdefg') → ab
-firstTwo('ab') → ab
+// firstTwo('Hello') → He
+// firstTwo('abcdefg') → ab
+// firstTwo('ab') → ab
+
+function firstTwo(str){
+    if(str.length < 2){
+    return str.charAt(0)
+    }if(str.length >= 2){
+    return str.charAt(0) + str.charAt(1)
+    }
+  }
