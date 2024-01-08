a7ee1e2af9a8a28f3b6a6a59462c799f15490fbf
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-08 22:30:19 +0100

    updated

--- strings1/extrafront.js
+++ strings1/extrafront.js
@@ -1,9 +1,17 @@
-String-1 -- extraFront
-Given a string, return a new string made of 3 copies of the first 2 chars of the original string. 
-The string may be any length. If there are fewer than 2 chars, use whatever is there.
+// String-1 -- extraFront
+// Given a string, return a new string made of 3 copies of the first 2 chars of the original string. 
+// The string may be any length. If there are fewer than 2 chars, use whatever is there.
 
-Examples
+// Examples
 
-extraFront('Hello') → HeHeHe
-extraFront('ab') → ababab
-extraFront('H') → HHH
+// extraFront('Hello') → HeHeHe
+// extraFront('ab') → ababab
+// extraFront('H') → HHH
+
+function extraFront(str){
+    if(str.length < 2){
+      return str.charAt(0)+ str.charAt(0)+ str.charAt(0)
+  }if(str.length >= 2){
+   return str.charAt(0)+str.charAt(1)+str.charAt(0)+str.charAt(1)+str.charAt(0)+str.charAt(1)
+  }
+  }
