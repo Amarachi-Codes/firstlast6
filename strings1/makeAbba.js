8899ce1f53fc0d032d8e5692b201011832ceef85
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-01-08 22:28:51 +0100

    updated question

--- strings1/makeAbba.js
+++ strings1/makeAbba.js
@@ -1,12 +1,12 @@
-String-1 -- makeAbba
-Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" 
-and "Bye" returns "HiByeByeHi".
+// String-1 -- makeAbba
+// Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" 
+// and "Bye" returns "HiByeByeHi".
 
-Examples
+// Examples
 
-makeAbba('Hi', 'Bye') → HiByeByeHi
-makeAbba('Yo', 'Alice') → YoAliceAliceYo
-makeAbba('What', 'Up') → WhatUpUpWhat
+// makeAbba('Hi', 'Bye') → HiByeByeHi
+// makeAbba('Yo', 'Alice') → YoAliceAliceYo
+// makeAbba('What', 'Up') → WhatUpUpWhat
 
 function makeAbba(a, b){
     return a+ b+b+a
